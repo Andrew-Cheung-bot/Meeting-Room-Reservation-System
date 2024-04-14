d29403070666fa6119134b7210615a4ba9c2d673
Potatomato <andrew.chu.cheung@gmail.com>
2024-04-15 05:38:40 +0800

    upload BookingForm

--- Mobile-project-8100/src/views/BookingPage.vue
+++ Mobile-project-8100/src/views/BookingPage.vue
@@ -6,37 +6,45 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <div v-if="!show">
+        <BookingForm />
+      </div>
+      <div v-if="show">
+        <ion-card>
+          <img alt="room A" src="/room_a.jpg" style="width: 100%; height: 400px;" />
+          <ion-card-header>
+            <ion-card-title>Room A</ion-card-title>
+            <ion-card-subtitle>4 seats provided</ion-card-subtitle>
+          </ion-card-header>
+        </ion-card>
 
-      <ion-card>
-        <img alt="room A" src="/room_a.jpg" style="width: 100%; height: 400px;" />
-        <ion-card-header>
-          <ion-card-title>Room A</ion-card-title>
-          <ion-card-subtitle>4 seats provided</ion-card-subtitle>
-        </ion-card-header>
-      </ion-card>
+        <ion-card>
+          <img alt="room B" src="/room_b.jpg" style="width: 100%; height: 400px;" />
+          <ion-card-header>
+            <ion-card-title>Room B</ion-card-title>
+            <ion-card-subtitle>6 seats provided</ion-card-subtitle>
+          </ion-card-header>
+        </ion-card>
 
-      <ion-card>
-        <img alt="room B" src="/room_b.jpg" style="width: 100%; height: 400px;" />
-        <ion-card-header>
-          <ion-card-title>Room B</ion-card-title>
-          <ion-card-subtitle>6 seats provided</ion-card-subtitle>
-        </ion-card-header>
-      </ion-card>
+        <ion-card>
+          <img alt="room C" src="/room_c.png" style="width: 100%; height: 400px;" />
+          <ion-card-header>
+            <ion-card-title>Room C</ion-card-title>
+            <ion-card-subtitle>10 seats provided</ion-card-subtitle>
+          </ion-card-header>
+        </ion-card>
 
-      <ion-card>
-        <img alt="room C" src="/room_c.png" style="width: 100%; height: 400px;" />
-        <ion-card-header>
-          <ion-card-title>Room C</ion-card-title>
-          <ion-card-subtitle>10 seats provided</ion-card-subtitle>
-        </ion-card-header>
-      </ion-card>
-
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup>
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
+import { ref } from 'vue'
+import BookingForm from '../components/BookingForm.vue';
+
+const show = ref(false)
 </script>
 
 <style scoped>
