7eca91f805cfe53b48ed96e6d45250b105e82d04
Rachelcrq <1336258708@qq.com>
2024-04-16 22:22:48 +0800

    add user crud

---
+++ Express.js-3000/utils/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const TOKEN_SECRET = 'comp7270_group_express';
+
+// generate a token
+const generateToken = function (user) {
+    return jwt.sign(user, TOKEN_SECRET, {
+        expiresIn: 86400 // expires in 24 hours
+    });
+}
+
+// 解码 Token
+const decodeToken = function (token) {
+    try {
+        const decoded = jwt.verify(token, TOKEN_SECRET);
+        return decoded;
+    } catch (err) {
+        return null; // Token 验证失败
+    }
+}
+
+module.exports = { generateToken, decodeToken };
